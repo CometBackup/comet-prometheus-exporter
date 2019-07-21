93edca5583fff07d0c121eec87256da596710a06
Mason <mason@cometbackup.com>
2019-07-22 10:47:47 +1200

    metrics: convert from hand-written exposition to using library

--- wwwroot/metrics/index.php
+++ wwwroot/metrics/index.php
@@ -12,6 +12,8 @@ $cs = new \Comet\Server(
     getenv('COMET_ADMIN_PASS')
 );
 
+$registry = new \Prometheus\CollectorRegistry(new \Prometheus\Storage\InMemory());
+
 
 // Load some basic information using the Comet Server API
 
@@ -22,74 +24,69 @@ $online_devices = $cs->AdminDispatcherListActive();
 $recentjobs = $cs->AdminGetJobsForDateRange(time() - 86400, time()); // Jobs from the last 24 hours
 
 
-// Build inverted index of online devices for traversal
+// Metric
+// Total number of users on the server
 
-$device_is_online_lookup = [];
+$gauge = $registry->getOrRegisterGauge('cometserver', 'total_users', 'The total number of users on this Comet Server', []);
+$gauge->set(count($users), []);
+
+
+// Metric
+// Categorise recent job counts, to report on them separately as well as in aggregate
+
+$recentjobs_gauge = $registry->getOrRegisterGauge("cometserver", "recentjobs", "Total number of jobs in the last 24 hours", ['status']);
+
+$recentjobs_gauge->set(0, ['success']);
+$recentjobs_gauge->set(0, ['running']);
+$recentjobs_gauge->set(0, ['warning']);
+$recentjobs_gauge->set(0, ['quota_exceeded']);
+$recentjobs_gauge->set(0, ['error']);
+
+foreach($recentjobs as $job) {
+    if ($job->Status >= \Comet\Def::JOB_STATUS_STOP_SUCCESS__MIN && $job->Status <= \Comet\Def::JOB_STATUS_STOP_SUCCESS__MAX) {
+        $recentjobs_gauge->inc(['success']);
+
+    } else if ($job->Status >= \Comet\Def::JOB_STATUS_RUNNING__MIN && $job->Status <= \Comet\Def::JOB_STATUS_RUNNING__MAX) {
+        $recentjobs_gauge->inc(['running']);
+
+    } else if ($job->Status == \Comet\Def::JOB_STATUS_FAILED_WARNING) {
+        $recentjobs_gauge->inc(['warning']);
+
+    } else if ($job->Status == \Comet\Def::JOB_STATUS_FAILED_QUOTA) {
+        $recentjobs_gauge->inc(['quota_exceeded']);
+
+    } else {
+        $recentjobs_gauge->inc(['error']);
+
+    }
+}
+
+
+// Metric
+// Online/offline status of each device
+
+$deviceGauge = $registry->getOrRegisterGauge('cometserver', 'device', "The online/offline status of each registered device", ['username', 'device_id', 'device_friendly_name']);
+
+$device_is_online_lookup = []; // Build inverted index of online devices for traversal
 foreach($online_devices as $live_connection) {
     $key = $live_connection->Username . "\x00" . $live_connection->DeviceID;
     $device_is_online_lookup[$key] = true;
 }
 
-
-// Categorise recent job counts, to report on them separately as well as in aggregate
-
-$recentjobs_success_ct = 0;
-$recentjobs_running_ct = 0;
-$recentjobs_warning_ct = 0;
-$recentjobs_quota_ct   = 0;
-$recentjobs_failure_ct = 0;
-foreach($recentjobs as $job) {
-    if ($job->Status >= \Comet\Def::JOB_STATUS_STOP_SUCCESS__MIN && $job->Status <= \Comet\Def::JOB_STATUS_STOP_SUCCESS__MAX) {
-        $recentjobs_success_ct++;
-
-    } else if ($job->Status >= \Comet\Def::JOB_STATUS_RUNNING__MIN && $job->Status <= \Comet\Def::JOB_STATUS_RUNNING__MAX) {
-        $recentjobs_running_ct++;
-
-    } else if ($job->Status == \Comet\Def::JOB_STATUS_FAILED_WARNING) {
-        $recentjobs_warning_ct++;
-
-    } else if ($job->Status == \Comet\Def::JOB_STATUS_FAILED_QUOTA) {
-        $recentjobs_quota_ct++;
-
-    } else {
-        $recentjobs_failure_ct++;
-
+foreach($users as $username => $user) {
+    foreach($user->Devices as $device_id => $device) {
+        $is_online = array_key_exists($username . "\x00" . $device_id, $device_is_online_lookup);
+        $deviceGauge->set($is_online ? 1 : 0, [$username, $device_id, $device->FriendlyName]);
     }
 }
 
 
-// Display the results in Prometheus text exposition format
-// @ref https://prometheus.io/docs/instrumenting/exposition_formats/ 
+// Render result
 
-header('Content-Type: text/plain; version=0.0.4');
-?>
-# HELP cometserver_total_users Total number of users on this Comet Server
-# TYPE cometserver_total_users gauge
-cometserver_total_users <?=count($users)?> 
+$renderer = new \Prometheus\RenderTextFormat();
+$result = $renderer->render($registry->getMetricFamilySamples());
 
-# HELP cometserver_recentjobs Total number of jobs in the last 24 hours
-# TYPE cometserver_recentjobs gauge
-cometserver_recentjobs{status="success"} <?=$recentjobs_success_ct?> 
-cometserver_recentjobs{status="running"} <?=$recentjobs_running_ct?> 
-cometserver_recentjobs{status="warning"} <?=$recentjobs_warning_ct?> 
-cometserver_recentjobs{status="quota_exceeded"} <?=$recentjobs_quota_ct?> 
-cometserver_recentjobs{status="error"} <?=$recentjobs_failure_ct?> 
+header('Content-Type: '.\Prometheus\RenderTextFormat::MIME_TYPE);
+echo $result;
 
-# HELP cometserver_device The online/offline status of each registered device
-# TYPE cometserver_device gauge
-<?php
-    foreach($users as $username => $user) {
-        foreach($user->Devices as $device_id => $device) {
-            echo 'cometserver_device{'.
-                'username=' . json_encode($username) . ', '.
-                'device_id=' . json_encode($device_id) . ', '.
-                'device_friendly_name=' . json_encode($device->FriendlyName) .
-            '}=' . (
-                array_key_exists($username . "\x00" . $device_id, $device_is_online_lookup)
-                    ? '1'
-                    : '0'
-            ).
-            "\n";
-        }
-    }
-?> 
+die();
